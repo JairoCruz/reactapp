0db90eb00c44158c5355d5104ecc74b659621fe9
JairoCruz <azaveliz@gmail.com>
2017-02-28 13:59:59 -0600

    la app es funcional

--- src/App.js
+++ src/App.js
@@ -8,12 +8,15 @@ class App extends Component {
   constructor () {
     super();
     this.state = {
-      user: null
+      user: null,
+      uploadValue: 0,
+      pictures: []
     }
 
     this.handleAuth = this.handleAuth.bind(this);
     this.handleLogout = this.handleLogout.bind(this);
     this.renderLoginButton = this.renderLoginButton.bind(this);
+    this.handleUpload = this.handleUpload.bind(this);
   }
 
   // Metodo nuevo en EMACScript 2016 buscar informacion sobre el.
@@ -21,6 +24,12 @@ class App extends Component {
     firebase.auth().onAuthStateChanged(user => {
       // esto tambien se puede escribir this.setState( { user: user } )
       this.setState({ user });
+    });
+
+    firebase.database().ref('pictures').on('child_added', snapshot =>{
+      this.setState({
+        pictures: this.state.pictures.concat(snapshot.val())
+      });
     });
   }
 
@@ -38,6 +47,32 @@ class App extends Component {
       .catch(error => console.log(`Error ${error.code}: ${error.message}`));
   }
 
+
+  handleUpload (event) {
+    const file = event.target.files[0];
+    const storageRef = firebase.storage().ref(`/fotos/${file.name}`);
+    const task = storageRef.put(file);
+
+    task.on('state_changed', snapshot => {
+      let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      this.setState({
+        uploadValue: percentage
+      })
+    }, error => {
+      console.log(error.message)
+    }, () => {
+      const record = {
+        photoURL: this.state.user.photoURL,
+        displayName: this.state.user.displayName,
+        image: task.snapshot.downloadURL
+      };
+
+      const dbRef = firebase.database().ref('pictures');
+      const newPicture = dbRef.push();
+      newPicture.set(record);
+    });
+  }
+
   renderLoginButton () {
     // si el usuario esta logueado
     if (this.state.user) {
@@ -46,7 +81,21 @@ class App extends Component {
           <img width="100" src={this.state.user.photoURL} alt={this.state.user.displayName} />
           <p>Hola {this.state.user.displayName}! </p>
           <button onClick={this.handleLogout}>Salir</button>
-          <FileUpload />
+          <br/>
+          <progress value={this.state.uploadValue} max="100"></progress>
+          <FileUpload onUpload={this.handleUpload} />
+
+          {
+            this.state.pictures.map(picture => (
+                <div>
+                  <img src={picture.image} alt="" />
+                  <br/>
+                  <img width="48" src={picture.photoURL} alt={picture.displayName} />
+                  <br/>
+                  <span>{picture.displayName}</span>
+                </div>
+              )).reverse()
+          }
         </div>
         );
     } else { 
@@ -63,9 +112,9 @@ class App extends Component {
         <div className="App-header">
           <h2>Welcome to PsGram</h2>
         </div>
-        <p className="App-intro">
+        <div className="App-intro">
           { this.renderLoginButton() }
-        </p>
+        </div>
       </div>
     );
   }
